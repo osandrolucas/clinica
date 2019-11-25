2ce4c3507b9d85aca2e97e2c126b4c752236e43a
M374LX <wilsalx@gmail.com>
2019-11-25 20:44:50 -0200

    Aplicação de recomendações do PMD

--- src/main/java/clinica/dao/GenericDao.java
+++ src/main/java/clinica/dao/GenericDao.java
@@ -1,7 +1,7 @@
 package clinica.dao;
 
 public interface GenericDao<T> {
-    public int insert(T obj);
-    public java.util.List<T> listAll();
-    public T findByID(Integer id);
+    int insert(T obj);
+    java.util.List<T> listAll();
+    T findByID(Integer id);
 }
